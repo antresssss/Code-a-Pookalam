631e8674f500170099289e9cabc4e5b260f31081
Andrea Tresa Tom <132264291+antresssss@users.noreply.github.com>
2024-09-21 22:52:51 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,85 +1,72 @@
 from turtle import *
 
-# Setup speed and background color
 speed(0)
 bgcolor('black')
 
-# Function to draw a petal
 def draw_petal(radius, color, angle=60):
-    pencolor('black')  # Outline color
+    pencolor('black')
     fillcolor(color)
     begin_fill()
-    circle(radius, angle)  # Draw arc for half petal
+    circle(radius, angle)
     lt(120)
     circle(radius, angle)
     lt(120)
     end_fill()
 
-# Function to draw a full flower with n petals
 def draw_flower(petal_count, petal_radius, petal_color):
     for _ in range(petal_count):
-        draw_petal(petal_radius, petal_color)  # Draw one petal
-        rt(360 / petal_count)  # Rotate to position the next petal
+        draw_petal(petal_radius, petal_color)
+        rt(360 / petal_count)
 
-# Draw concentric circles with a floral touch
 def draw_concentric_flower():
     up()
     goto(0, -150)
     down()
 
-    # First circle with orange petals
     pen(fillcolor="orange", pencolor="magenta", pensize=2)
     begin_fill()
-    circle(150)  # Outer circle
+    circle(150)
     end_fill()
 
-    # First set of petals
     up()
     goto(0, -120)
     down()
-    draw_flower(12, 100, 'red')  # Red petals around the first circle
+    draw_flower(12, 100, 'red')
 
-    # Second circle with purple
     up()
     goto(0, -90)
     down()
     pen(fillcolor="purple", pencolor="white", pensize=2)
     begin_fill()
-    circle(90)  # Middle circle
+    circle(90)
     end_fill()
 
-    # Second set of petals (pink)
     up()
     goto(0, -70)
     down()
-    draw_flower(10, 70, 'pink')  # Pink petals around the second circle
+    draw_flower(10, 70, 'pink')
 
-    # Third circle with magenta
     up()
     goto(0, -50)
     down()
     pen(fillcolor="magenta", pencolor="white", pensize=2)
     begin_fill()
-    circle(50)  # Inner circle
+    circle(50)
     end_fill()
 
-    # Third set of petals (white)
     up()
     goto(0, -40)
     down()
-    draw_flower(8, 40, 'white')  # White petals around the third circle
+    draw_flower(8, 40, 'white')
 
-# Central soft flower
 def draw_center_flower():
     up()
     goto(0, 0)
     down()
-    draw_flower(6, 30, 'purple')  # Soft central purple flower
+    draw_flower(6, 30, 'purple')
 
-# Call the functions to draw the full pattern
-draw_concentric_flower()  # Draw the concentric flower patterns
-draw_center_flower()  # Draw the central soft flower
+draw_concentric_flower()
+draw_center_flower()
 
-# Hide the turtle and complete
 ht()
 exitonclick()
